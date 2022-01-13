0c4d99d53aa6169a9e62c3e2f6c05dbcc0114734
jwyc <janis.wy.chan@gmail.com>
2022-01-13 00:59:54 -0800

    profileInfo div added with information from api placed in html and append to overview class, called in getData

--- js/script.js
+++ js/script.js
@@ -7,6 +7,31 @@ const getData = async function(){
    const res = await fetch(`https://api.github.com/users/${username}`);
    const data = await res.json();
    console.log(data);
+   userProfileInfo(data);
 };
 
 getData();
+
+const userProfileInfo = function(data){
+   let profileDiv= document.createElement("div");
+   profileDiv.classList.add("user-info");
+   profileDiv.innerHTML =`   
+   <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+   </figure>
+   <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+   </div>`
+
+   overview.append(profileDiv);
+   
+};
+
+
+
+
+
+
