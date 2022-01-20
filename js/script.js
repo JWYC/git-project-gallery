90996a135af7945bfecfcba8d165b11bdb854235
jwyc <janis.wy.chan@gmail.com>
2022-01-20 00:32:47 -0800

    fix public number count

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ const userProfileInfo = function(data){
       <p><strong>Name:</strong> ${data.name}</p>
       <p><strong>Bio:</strong> ${data.bio}</p>
       <p><strong>Location:</strong> ${data.location}</p>
-      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos-4}</p>
    </div>`
 
    overview.append(profileDiv);
